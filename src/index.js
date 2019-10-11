6624cdd125c34697d25173dff1ca76b8085034e2
Svistov Nikolay <swistov@gmail.com>
2019-10-11 14:25:11 +0300

    Fetch API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+
+const getResource = async (url) => {
+    const res = await fetch(url);
+    const body = await res.json();
+    return body;
+};
+
+getResource('https://swapi.co/api/people/1/')
+    .then((body) => {
+        console.log(body);
+    });

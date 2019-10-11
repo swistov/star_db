89d5152769f78be9305e23074f6cb9349b4394cd
Svistov Nikolay <swistov@gmail.com>
2019-10-11 14:32:31 +0300

    Fetch API error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 
 const getResource = async (url) => {
     const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}` +
+        `, received ${res.status}`);
+    }
+
     const body = await res.json();
     return body;
 };
 
-getResource('https://swapi.co/api/people/1/')
+getResource('https://swapi.co/api/people/1wer/')
     .then((body) => {
         console.log(body);
+    })
+    .catch((err) => {
+        console.error('Could not fetch', err);
     });

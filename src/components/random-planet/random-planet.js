ec9a8d7d09677135d0287265d4abb0ea32d4e9a2
Svistov Nikolai <swistov@gmail.com>
2019-10-15 22:43:01 +0300

    l59

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -10,7 +10,8 @@ export default class RandomPlanet extends Component {
     swapiService = new SwapiService();
 
     state = {
-        planet: {}
+        planet: {},
+        loading: true
     };
 
     constructor() {
@@ -19,7 +20,10 @@ export default class RandomPlanet extends Component {
     }
 
     onPlanetLoaded = (planet) => {
-        this.setState({ planet });
+        this.setState({
+            planet,
+            loading: false
+        });
     };
 
     updatePlanet() {
@@ -31,34 +35,47 @@ export default class RandomPlanet extends Component {
 
     render() {
 
-        const { planet: { id, name, population,
-            rotationPeriod, diameter }  } = this.state;
+        const { planet, loading } = this.state;
 
-        return <Spinner/>;
+        if (loading) {
+            return <Spinner />;
+        }
 
         return (
             <div className="random-planet jumbotron rounded">
-                <img className="planet-image"
-                     src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
-                <div>
-                    <h4>{ name }</h4>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <span className="term">Population</span>
-                            <span>{ population }</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Rotation Period</span>
-                            <span>{ rotationPeriod }</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Diameter</span>
-                            <span>{ diameter }</span>
-                        </li>
-                    </ul>
-                </div>
+                <PlanetView planet={planet} />
             </div>
 
         );
     }
 }
+
+const PlanetView = ({ planet }) => {
+
+    const { id, name, population,
+        rotationPeriod, diameter } = planet;
+
+    return (
+        <React.Fragment>
+            <img className="planet-image"
+                 src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+            <div>
+                <h4>{ name }</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">Population</span>
+                        <span>{ population }</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Rotation Period</span>
+                        <span>{ rotationPeriod }</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Diameter</span>
+                        <span>{ diameter }</span>
+                    </li>
+                </ul>
+            </div>
+        </React.Fragment>
+    )
+};
